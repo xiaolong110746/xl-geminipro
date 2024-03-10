ebcd3db817b662c788a459c428b015870b9d98dd
xiaolong110746 <158761093+xiaolong110746@users.noreply.github.com>
2024-03-10 14:59:18 +0800

    Update getAiModel.tsx

--- src/helpers/getAiModel.tsx
+++ src/helpers/getAiModel.tsx
@@ -9,6 +9,6 @@ export const getAiModel = (
 ) =>
     ai.getGenerativeModel({
         ...options,
-        model: "gemini-4.0-pro-latest",
+        model: "gemaini-4.0-pro-latest",
         apiVersion: "v1",
     });
